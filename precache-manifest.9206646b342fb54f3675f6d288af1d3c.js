b81c02e48001f5dd931cad22a74335b2f0240d74
jhedgpeth <jeff@brainspank.net>
2019-06-23 09:53:26 -0500

    Updates

---
+++ precache-manifest.9206646b342fb54f3675f6d288af1d3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4a986d09689809b52a389c2ab63f30",
+    "url": "/react-game4/index.html"
+  },
+  {
+    "revision": "d4ae983e226c13821082",
+    "url": "/react-game4/static/css/main.62d9d8e7.chunk.css"
+  },
+  {
+    "revision": "1483b424aa5aab585d7c",
+    "url": "/react-game4/static/js/2.96eaf142.chunk.js"
+  },
+  {
+    "revision": "d4ae983e226c13821082",
+    "url": "/react-game4/static/js/main.aa58764f.chunk.js"
+  },
+  {
+    "revision": "08c32d3994e811585365",
+    "url": "/react-game4/static/js/runtime~main.d1f90428.js"
+  }
+]);
